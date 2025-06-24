169e39f5927b604e2ed73c8a5813b6c3483e6f49
Damian Kubiczek <d.kubiczek@reply.com>
2025-06-24 23:34:26 +0200

    project structure updated

--- RyanVibes2/jest.config.js
+++ RyanVibes2/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
   },
   testMatch: ['**/scheduleParser.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
