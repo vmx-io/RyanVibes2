f39466a4d34444c81937c848554ca8a51860e4ac
Damian Kubiczek <d.kubiczek@reply.com>
2025-06-26 20:18:14 +0200

    Favourites functionality

--- src/app/workbook/workbook.component.ts
+++ src/app/workbook/workbook.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeeService, Employee } from '../services/employee.service';
 import { CalendarComponent } from '../calendar/calendar.component';
+import { CookieService } from '../cookie.service';
 
 @Component({
   selector: 'app-workbook',
@@ -19,13 +20,31 @@ import { CalendarComponent } from '../calendar/calendar.component';
           class="employee-dropdown"
         >
           <option value="" disabled selected class="placeholder-option">Select an employee</option>
-          <option 
-            *ngFor="let employee of employees" 
-            [value]="employee.id"
-          >
-            {{ employee.firstName }} {{ employee.lastName }}
-          </option>
+          <ng-container *ngIf="favouriteEmployees.length">
+            <optgroup label="Favourites">
+              <option 
+                *ngFor="let employee of favouriteEmployees" 
+                [value]="employee.id"
+              >
+                {{ employee.firstName }} {{ employee.lastName }}
+              </option>
+            </optgroup>
+          </ng-container>
+          <ng-container *ngIf="otherEmployees.length">
+            <optgroup label="Others">
+              <option 
+                *ngFor="let employee of otherEmployees" 
+                [value]="employee.id"
+              >
+                {{ employee.firstName }} {{ employee.lastName }}
+              </option>
+            </optgroup>
+          </ng-container>
         </select>
+        <button *ngIf="selectedEmployee" (click)="toggleFavourite()" class="btn-favourite">
+          <span *ngIf="isFavourite(selectedEmployee.id)">★ Remove from Favourites</span>
+          <span *ngIf="!isFavourite(selectedEmployee.id)">☆ Add to Favourites</span>
+        </button>
       </div>
       <div *ngIf="!employees.length" class="no-data">
         <p>No employee data available. Please upload a file first.</p>
@@ -44,20 +63,31 @@ export class WorkbookComponent implements OnInit {
   employees: Employee[] = [];
   selectedEmployeeId: string = '';
   selectedEmployee: Employee | null = null;
+  favouriteIds: string[] = [];
+  favouriteEmployees: Employee[] = [];
+  otherEmployees: Employee[] = [];
 
   constructor(
     private employeeService: EmployeeService,
-    private router: Router
+    private router: Router,
+    public cookieService: CookieService
   ) {}
 
   ngOnInit(): void {
+    this.favouriteIds = this.getFavouritesFromCookie();
     this.employeeService.getEmployeesObservable().subscribe(employees => {
       this.employees = employees;
       if (employees.length === 0) {
         // If no employees, redirect to home
         this.router.navigate(['/']);
       }
+      this.splitEmployees();
     });
+  }
+
+  splitEmployees(): void {
+    this.favouriteEmployees = this.employees.filter(e => this.favouriteIds.includes(e.id));
+    this.otherEmployees = this.employees.filter(e => !this.favouriteIds.includes(e.id));
   }
 
   onEmployeeChange(): void {
@@ -70,5 +100,35 @@ export class WorkbookComponent implements OnInit {
 
   goToHome(): void {
     this.router.navigate(['/']);
+  }
+
+  toggleFavourite(): void {
+    if (!this.selectedEmployee) return;
+    const id = this.selectedEmployee.id;
+    if (this.isFavourite(id)) {
+      this.favouriteIds = this.favouriteIds.filter(fid => fid !== id);
+    } else {
+      this.favouriteIds.push(id);
+    }
+    this.setFavouritesToCookie(this.favouriteIds);
+    this.splitEmployees();
+  }
+
+  isFavourite(id: string): boolean {
+    return this.favouriteIds.includes(id);
+  }
+
+  getFavouritesFromCookie(): string[] {
+    const cookie = this.cookieService.get('favouriteEmployees');
+    if (!cookie) return [];
+    try {
+      return JSON.parse(cookie);
+    } catch {
+      return [];
+    }
+  }
+
+  setFavouritesToCookie(ids: string[]): void {
+    this.cookieService.set('favouriteEmployees', JSON.stringify(ids), 365);
   }
 } 
